fc46017e3f20374041cd8a43733f38aaebf1c203
yolentaaa <80032364+yolentaaa@users.noreply.github.com>
2021-03-04 16:37:48 +0700

    menentukan bilangan - atau +

---
+++ pertama.js
@@ -0,0 +1,8 @@
+alert('program memasukan angka positif dan negatif');
+let angka = prompt('masukan angka');
+
+if (angka >= 0) {
+  alert('angka ini positif');
+} else {
+  alert('angka ini negatif');
+}
